61b039aa474ca343ef0ca49a259f7c506280b78a
fabiokaz <fabiokaz@gmail.com>
2024-10-28 10:30:54 -0300

    feat - adjustments from spring boot framework.

---
+++ src/main/java/com/kaz/townsq/TownSqBackendTechChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.kaz.townsq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TownSqBackendTechChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TownSqBackendTechChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kaz/townsq/TownSqBackendTechChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaz.townsq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TownSqBackendTechChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
